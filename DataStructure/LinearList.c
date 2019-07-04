efff1f2a30c95354c69daa83e207da88feac41fc
holy_joon <ssjun511@gmail.com>
2019-07-05 00:58:19 +0900

    LinearList 1D

---
+++ DataStructure/LinearList.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int i, rec[4]={90,99,100,94};
+    for(i = 0; i<4; i++){
+        printf("The Score %d : %d\n", i+1, rec[i]);
+    }
+    return 0;
+}
+//Linear List for 1 Dimension.
--- README.md
+++
@@ -1,2 +0,0 @@
-This is .. I think test!
-and change the file
